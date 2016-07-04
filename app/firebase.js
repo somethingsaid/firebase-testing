f1ddbae63246b7e9532da36fda2e7f313b599996
somethingsaid <kwong.billy@gmail.com>
2016-07-04 16:00:47 -0400

    First commit

---
+++ app/firebase.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDybrBQ45HwC0bMzCX0F20uZommMd0GKS4",
+  authDomain: "fir-test-48c8c.firebaseapp.com",
+  databaseURL: "https://fir-test-48c8c.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
